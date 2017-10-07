0d80a176b96286c372851f74be4a81ba77400f29
DESKTOP-QV2I800\AriSurfacePro <ari.mouzakis1@gmail.com>
2017-10-07 15:44:20 +1100

    Initial commit

---
+++ app/src/main/java/com/mouzakis/arisurfacepro/studentmessagingapplication/RegisterAccount.java
@@ -0,0 +1,17 @@
+package com.mouzakis.arisurfacepro.studentmessagingapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterAccount extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_account);
+    }
+
+}
